2cb7484ec09bd789d7c37501edf04b9c4e49831e
Dan Yang <dsyang92@gmail.com>
2013-12-22 16:38:19 -0500

    Frames

---
+++ js/stage.js
@@ -0,0 +1,184 @@
+function Stage(canvas) {
+		this.canvas = canvas;
+		this.context = canvas.getContext('2d');
+		this.context.font = "Bold 16px sans-serif";
+		this.context.textBaseline = "center";
+		this.context.textAlign = "center";
+
+		//In case browser doesn't support dashed lines
+		if (!this.context.setLineDash) {
+				this.context.setLineDash = function() {};
+		}
+
+		this.frameCount = 1;
+		this.frame = 0;
+
+		this.markers = [];
+		this.markerLayout = [0,0];
+		this.markerHorzDist = 0;
+		this.markerVertDist = 0;
+
+		this.dancerCount = 0;
+		this.dancers = new DancersList(document.getElementById('dancers'));
+
+		var self = this;
+		this._listeners = {
+				mousemove: function() { self._onMouseMove.apply(self, arguments); },
+				mousedown: function() { self._onMouseDown.apply(self, arguments); },
+				mouseup: function() { self._onMouseUp.apply(self, arguments); },
+				click: function() { self._onClick.apply(self, arguments); }
+		};
+		var type;
+		for (type in this._listeners) {
+				this.canvas.addEventListener(type, this._listeners[type]);
+		}
+}
+
+Stage.prototype.serialize = function() {
+		var stageObject = {
+				markerLayout: this.markerLayout,
+				dancerCount: this.dancerCount,
+				dancers: [],
+				frameCount: this.frameCount
+		};
+		var id;
+		for (id in this.dancers.array) {
+				stageObject.dancers[id] =
+						this.dancers.array[id].serialize(this.markerHorzDist,
+																						 this.markerVertDist);
+		}
+
+		return escape(JSON.stringify(stageObject));
+
+}
+
+Stage.deserializeInto = function(str, stage) {
+		if(str[str.length-1] === '/') {
+				str[str.length-1] = ' ';
+		}
+		var stageObject = JSON.parse(unescape(str));
+		stage.frameCount = stageObject.frameCount;
+		stage.frame = 0;
+
+		stage.setMarkers(stageObject.markerLayout[0], stageObject.markerLayout[1]);
+		stage.dancers = new DancersList(stage.dancers.element);
+		var id;
+		for (id in stageObject.dancers) {
+				var o = stageObject.dancers[id];
+				stage.addDancer(o.name, o.gender, o.color);
+				stage.dancers.array[id].xs = o.posxs;
+				stage.dancers.array[id].ys = o.posys;
+		}
+}
+
+Stage.prototype.draw = function() {
+		var id1, id2;
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		for (id1 in this.markers) {
+				this.markers[id1].draw(this);
+		}
+
+		for (id2 in this.dancers.array) {
+				if(this.dancers.array[id2] === this.dancers.active) {
+						continue;
+				}
+				this.dancers.array[id2].draw(this);
+		}
+
+		//always draw active dancer last
+		this.dancers.active && this.dancers.active.draw(this);
+}
+
+Stage.prototype.setMarkers = function(numHorz, numVert) {
+		this.markers = [];
+		this.markerLayout = [numHorz, numVert];
+
+		var spaceHorz = this.canvas.width / (2*(numHorz+1));
+		var spaceVert = this.canvas.height / (2*(numVert+1));
+
+		this.markerHorzDist = spaceHorz;
+		this.markerVertDist = spaceVert;
+
+		for (var i = 1; i < 2*(numHorz+1); i++) {
+				// if i even, then actual marker, else it's a mid
+				console.log("pushing horz");
+				var m = new Marker(spaceHorz*i, 0, (i % 2 === 0));
+
+				this.markers.push(m);
+		}
+		for (var j = 1; j < 2*(numVert+1); j++) {
+				// if j even, then actual marker, else it's a mid
+				var m = new Marker(0, spaceVert*j, (j % 2 === 0));
+
+				this.markers.push(m);
+		}
+}
+
+Stage.prototype.addDancer = function(name, gender, color) {
+		this.dancerCount++;
+		var self = this;
+		var dancer = new Dancer(this.dancerCount, name, gender, color);
+		dancer.element.mousedown(function() {
+				if (self.dancers.active === dancer) {
+						self.dancers.makeInactive();
+				} else {
+						self.dancers.makeActive(dancer)
+				}
+		});
+		this.dancers.push(dancer);
+}
+
+Stage.prototype.snapH = function(x) {
+		return Math.floor((x+25)/(this.markerHorzDist/2))*(this.markerHorzDist/2);
+}
+
+Stage.prototype.snapV = function(x) {
+		return Math.floor((x+25)/(this.markerVertDist/2))*(this.markerVertDist/2);
+}
+
+Stage.prototype._onMouseMove = function(event) {
+		var pos = offsets(event);
+		var f = this.frame;
+		if (this.dancers.active !== null)  {
+				var dancer = this.dancers.active;
+				dancer.xs[f] = this.snapH(pos.x-8);
+				dancer.ys[f] = this.snapV(pos.y-8);
+		} else {
+				var insideSomeone = false;
+				for (id in this.dancers.array) {
+						var d = this.dancers.array[id];
+						if( insideDancer(d,f,pos.x,pos.y) ) {
+								insideSomeone = true;
+								this._focused = d;
+						}
+				}
+				if(!insideSomeone) this._focused = null;
+		}
+
+		this.draw();
+		if (this._focused) {
+				console.log("focused on: ", this._focused.name);
+		}
+
+}
+Stage.prototype._onMouseDown = function(event) {
+		//console.log("mousedown");
+}
+Stage.prototype._onMouseUp = function(event) {
+//		console.log("mouseup");
+}
+Stage.prototype._onClick = function(event) {
+		var pos = offsets(event);
+		var f = this.frame;
+
+		var insideSomeone = false;
+		for (id in this.dancers.array) {
+				var d = this.dancers.array[id];
+				if( insideDancer(d,f,pos.x,pos.y) && d !== this.dancers.active) {
+						insideSomeone = true;
+						this.dancers.makeActive(d);
+				}
+		}
+		if (!insideSomeone) this.dancers.makeInactive();
+
+}
---
+++ js/ui.js
@@ -0,0 +1,80 @@
+$("#display-names").change(function() {
+		stage.displayNames = !stage.displayNames;
+		stage.draw();
+});
+
+$("#display-grid").change(function() {
+		stage.displayGrid = !stage.displayGrid;
+		stage.draw();
+});
+
+$("#previous-frame").click(function() {
+		if (stage.frame > 0) {stage.frame--;}
+		updateFrameCountUI();
+		stage.draw();
+});
+
+$("#next-frame").click(function() {
+		if (stage.frame+1 < stage.frameCount) {
+		stage.frame++;
+    }
+		updateFrameCountUI();
+		stage.draw();
+});
+
+$("#make-stage-image").click(function() {
+		var str = stage.canvas.toDataURL();
+		document.getElementById("stage-image").src = str;
+});
+
+$("#clear-stage").click(function() {
+		var id;
+		var f = stage.frame;
+		stage.dancers.makeInactive();
+		for (id in stage.dancers.array) {
+				stage.dancers.array[id].xs[f] = undefined;
+				stage.dancers.array[id].ys[f] = undefined;
+		}
+});
+
+$("#num-marker-input-submit").click(function() {
+		var numHorz = parseInt($("#num-marker-input-horz").val(), 10);
+		var numVert = parseInt($("#num-marker-input-vert").val(), 10);
+		console.log("Setting", numHorz, numVert);
+		stage.setMarkers(numHorz, numVert);
+		stage.draw();
+});
+
+$("#formation-add-frame").click(function() {
+		stage.frameCount++;
+		var newf = stage.frameCount-1;
+		var prevf = newf-1;
+
+		stage.dancers.makeInactive();
+		for (id in stage.dancers.array) {
+				stage.dancers.array[id].xs[newf] =
+						stage.dancers.array[id].xs[prevf];
+				stage.dancers.array[id].ys[newf] =
+						stage.dancers.array[id].ys[prevf];
+		}
+		stage.frame = newf;
+
+		updateFrameCountUI();
+		stage.draw();
+});
+
+$("#formation-save-formation").click(function() {
+		var stageString = stage.serialize();
+		window.location.search = "?stage="+stageString;
+});
+
+$("#formation-clear-all").click(function() {
+		window.location.search = "";
+});
+
+$("#addDancerButton").click(function() {
+		var name = $("#addDancerName").val();
+  	var gender = $("#addDancerGender").val()[0];
+  	var color = $("#addDancerColor").val().toLowerCase();
+  	stage.addDancer(name, gender, color);
+});
